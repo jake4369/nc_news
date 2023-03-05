07f8fb757ad1fe10d276e180b442dc838fcedbf5
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-03-05 15:55:56 +0000

    Add deleteComment to commentsModel

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -66,3 +66,25 @@ exports.addCommentToArticle = (articleId, username, body) => {
       }
     });
 };
+
+exports.deleteComment = (commentId) => {
+  return db
+    .query(
+      `
+      DELETE FROM comments
+      WHERE comment_id = $1
+      RETURNING *;
+    `,
+      [commentId]
+    )
+    .then((result) => {
+      if (result.rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Comment not found",
+        });
+      }
+      const comment = result.rows[0];
+      return comment;
+    });
+};
