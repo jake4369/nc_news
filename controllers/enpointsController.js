3f8d169d8fe7ff733ef5db7aa59070053c36e365
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-03-05 16:55:06 +0000

    Deleted controllers/enpointsController.js

--- controllers/enpointsController.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs/promises");
-
-exports.getEndpoints = () => {
-  return fs
-    .readFile(`${__dirname}/../endpoints.json`, "utf8")
-    .then((endpoints) => {
-      return JSON.parse(endpoints);
-    });
-};
