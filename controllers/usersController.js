a76f58a153f0ca96b4884cc9e845a404eb2aa8a0
Jake Samuels <samuels242@googlemail.com>
2023-04-10 15:33:48 +0100

    Add controller for getUser

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,3 +8,13 @@ exports.getAllUsers = (req, res, next) => {
     })
     .catch((error) => next(error));
 };
+
+exports.getUser = (req, res, next) => {
+  const { username } = req.params;
+  usersModel
+    .getUser(username)
+    .then((user) => {
+      res.status(200).json({ user });
+    })
+    .catch((error) => next(error));
+};
