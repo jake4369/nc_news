29b405148b3d2e5fc78482ad7d0219e22763e060
Jake Samuels <samuels242@googlemail.com>
2023-04-10 15:33:22 +0100

    Add model for getUser

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,14 +1,37 @@
 const db = require("./../db/connection");
 
+// Get all users
 exports.getAllUsers = () => {
   return db
     .query(
       `
             SELECT * FROM users;
-        `
+      `
     )
     .then((results) => {
       const users = results.rows;
       return users;
+    });
+};
+
+// Get user by username
+exports.getUser = (username) => {
+  return db
+    .query(
+      `
+      SELECT * FROM users 
+      WHERE username = $1;
+    `,
+      [username]
+    )
+    .then((results) => {
+      if (results.rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "No user found",
+        });
+      }
+      const user = results.rows[0];
+      return user;
     });
 };
